7515440f82702fa4a054e696fb06d3e3fe352c31
gabriel diniz gisoldo <gabriel@host.domain>
2015-10-21 22:36:53 -0200

    insert

---
+++ insert.sql
@@ -0,0 +1,4 @@
+insert into usuarios (email,senha,id_tipo_usuario) values ('gabrielgisoldi@gmail.com','1e536bdb71098aa3e6fed796efcd890ac746cb6b',1),('rafael.zulli0@gmail.com','601f1889667efaebb33b8c12572835da3f027f78',1),('gabrielgisoldi@hotmail.com','7c4a8d09ca3762af61e59520943dc26494f8941b',3),('developers@agenda.com','f7c3bc1d808e04732adf679965ccc34ca7ae3441',2);
+
+
+insert into tipo_usuarios (descricao) values ('administrador', 'grupo', 'comum');
